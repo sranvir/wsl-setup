92f3da145c8aeedd9edf07897b19f7fc8ef66ec2
Ranvir Singh <rnvs@iranvir.com>
2020-10-03 09:12:18 +0530

    Moved the vhdx directory to Windows Home directory

--- install_Ubuntu.ps1
+++ install_Ubuntu.ps1
@@ -3,7 +3,9 @@ ubuntu2004.exe
 wsl exec env PWD='/mnt/c/Users/r/wsl_setup' ./wsl_setup.sh
 wsl --shutdown
 wsl --export Ubuntu-20.04 .\Ubuntu.tar
+
+## Packaging everything away from Ubuntu App
 Remove-AppxPackage CanonicalGroupLimited.Ubuntu20.04onWindows_2004.2020.424.0_x64__79rhkp1fndgsc
-mkdir C:\Ubuntu
-wsl --import Ubuntu C:\Ubuntu .\Ubuntu.tar
+mkdir $HOME\Ubuntu
+wsl --import Ubuntu $HOME\Ubuntu .\Ubuntu.tar
 rm .\Ubuntu.tar
